a064c9c24a718b15355afc54661a93d081e6d266
amirbg2004 <amirbg43@hotmail.com>
2025-01-11 12:21:51 +0200

    Adjusted landing page header responsiveness using flex basis, grow, and shrink. Looks better on smaller screens. No overflow issues any longer.

--- views/landing_page/landing_page_header.php
+++ views/landing_page/landing_page_header.php
@@ -1,10 +1,10 @@
-<header class="flex w-full items-center justify-between p-6 bg-slate-600">
-	<h1 class="text-white font-bold">
+<header class="flex w-screen items-center justify-between p-6 px-12 bg-slate-600">
+	<h1 class="flex-1 basis-8/12 text-white font-bold text-nowrap">
 		Bite Builder Logo
 	</h1>
-	<div class="flex w-1/4 items-center justify-around ">
+	<div class="flex flex-1 basis-4/12 flex-wrap md:flex-nowrap lg:basis-2/12 items-center space-x-12">
 		<button id="login-signup-button"
-                class="text-white font-semibold bg-slate-900 p-6 px-12 rounded-lg transition hover:bg-slate-950">
+                class="flex-1 text-white font-semibold bg-slate-900 transition p-6 rounded-3xl hover:scale-95">
 			Log In / Sign Up
 		</button>
 		<?php include("../global/theme_switch_button.php"); ?>

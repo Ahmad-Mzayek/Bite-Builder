8aa83c92a3a448cd2d60c30a0cc1b7ab7d4bb384
amirbg2004 <amirbg43@hotmail.com>
2025-01-14 21:38:03 +0200

    Added the final bug-free version of login_controller.php .

--- controllers/login_controller.php
+++ controllers/login_controller.php
@@ -1,18 +1,29 @@
 <?php
-include_once 'database.php';  
+include('../database.php');  
+
+header("Content-Type: application/json");
 
 $response = '';
 
-if (isset($_POST['LOGIN'])) {
-    $input = trim($_POST['email']);
+if ($_SERVER["REQUEST_METHOD"] === "POST") {
+    $input = trim($_POST['login']);
     $password = trim($_POST['password']);
 
+    if (empty($input) || empty($password)) {
+        echo json_encode([
+            'status' => 'error',
+            'message' => "Credentials cannot be blank!"
+        ]);
+        exit;
+    }
+       
+
     if (filter_var($input, FILTER_VALIDATE_EMAIL)) {
-        $query = "SELECT * FROM users WHERE email =?";
+        $query = "SELECT * FROM users WHERE email = ?";
         $stmt = $mysqli->prepare($query);
      } 
      else {
-        $query = "SELECT * FROM users WHERE username =?";
+        $query = "SELECT * FROM users WHERE username = ?";
         $stmt = $mysqli->prepare($query);
     }
 
@@ -24,34 +35,47 @@ if (isset($_POST['LOGIN'])) {
         
         if ($result->num_rows === 1) {
             $user = $result->fetch_array();
-            
-            if (password_verify($password, $user['password'])) {
+
+            if (hash('sha256',$password) === $user['hashed_password']) {
                 $response = "Login successful!";
-                
                 session_start();
                 $_SESSION['user_logged_in'] = true;
                 $_SESSION['user_email'] = $input;
                 
                 // Sending response
-                echo json_encode(['status' => 'success', 'message' => $response]);
+                echo json_encode([
+                    'status' => 'success',
+                    'message' => $response
+                    ]);
                 exit;
             } 
             else {
-                $response = "Incorrect password!";
-                echo json_encode(['status' => 'error', 'message' => $response]);
+                $response = "Incorrect username or password!";
+                echo json_encode([
+                    'status' => 'error',
+                    'message' => $response]);
+                exit;
             }
         } else {
-            $response = "No user found!";
-            echo json_encode(['status' => 'error', 'message' => $response]);
+            $response = "Incorrect username or password.";
+            echo json_encode([
+                'status' => 'error',
+                'message' => $response
+            ]);
+            exit;
         }
 
         $stmt->close();
     } 
     else {
         $response = "Database query error.";
-        echo json_encode(['status' => 'error', 'message' => $response]);
+        echo json_encode([
+        'status' => 'error',
+        'message' => $response
+        ]);
+        exit;
     }
 }
 
-$conn->close();
+$mysqli->close();
 ?>

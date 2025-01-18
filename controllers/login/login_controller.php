2177f56218349ce001e23c46a699d6dc82bb2bf8
Ahmad Mzayek <ahmadmzayek.cs@gmail.com>
2025-01-18 13:09:00 +0200

    - Moved the session starting and the response sending to the main login controller file. - Made it more robust using try-catch.

---
+++ controllers/login/login_controller.php
@@ -0,0 +1,16 @@
+<?php // Main.
+include("login_controller_utils.php");
+
+header("Content-Type: application/json");
+
+try
+{
+    $username = handle_login();
+    start_user_session($username);
+    send_response("success", "Login successful!");
+}
+catch (Exception $exception)
+{
+    send_response('error', $exception->getMessage());
+}
+?>

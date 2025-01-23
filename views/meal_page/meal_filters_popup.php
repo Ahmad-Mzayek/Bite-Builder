03449647dbe0d8aad0ed2926444561fedd4901be
Ahmad Mzayek <ahmadmzayek.cs@gmail.com>
2025-01-23 19:27:15 +0200

    - Added more requirements.

--- views/meal_page/meal_filters_popup.php
+++ views/meal_page/meal_filters_popup.php
@@ -2,4 +2,5 @@
 <!-- Sort Options: Ascending - Descending -->
 <!-- Filters: Booleans, Checkboxes, same as Database, all checked by default -->
 <!-- Filters: Meal Categories, Checkboxes, same as Database (meal_category Table), all checked by default -->
+<!-- Give the user the option to view only meals added to favorites -->
 <!-- Design to be discussed on campus  -->

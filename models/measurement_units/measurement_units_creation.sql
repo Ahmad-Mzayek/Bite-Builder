1cdf048149332548db754cea18497a86813f48b8
Ahmad Mzayek <ahmadmzayek.cs@gmail.com>
2025-01-25 22:21:36 +0200

    - Removed the column "id". - Made the column "name_singular" the primary key.

--- models/measurement_units/measurement_units_creation.sql
+++ models/measurement_units/measurement_units_creation.sql
@@ -1,8 +1,7 @@
 CREATE TABLE IF NOT EXISTS measurement_units
 (
-    id              INT             NOT NULL    AUTO_INCREMENT,
     name_singular   VARCHAR(16)     NOT NULL    UNIQUE,
     name_plural     VARCHAR(16)     NOT NULL    UNIQUE,
 
-    CONSTRAINT primary_key_measurement_units PRIMARY KEY (id)
+    CONSTRAINT primary_key_measurement_units PRIMARY KEY (name_singular)
 );

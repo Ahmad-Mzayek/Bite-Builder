90959d477abef79846fdaaf46f667fdf0ab9a932
Ahmad Mzayek <ahmadmzayek.cs@gmail.com>
2025-01-25 21:32:33 +0200

    - Removed the column "dietary_filters_id". - Made the column "id" a foreign key that references the primary key of the table "dietary_filters". - Removed "AUTO_INCREMENT" from the column "id". - Renamed the column "email" to "email_address". - Added "UNIQUE" to the columns "id", "username", "email_address", and "phone_number".

--- models/users/users_creation.sql
+++ models/users/users_creation.sql
@@ -1,12 +1,13 @@
 CREATE TABLE IF NOT EXISTS users
 (
-   id                      INT            NOT NULL    AUTO_INCREMENT,
-   username                VARCHAR(32)    NOT NULL,
-   email                   VARCHAR(64)    NOT NULL,
-   phone_number            VARCHAR(16),
+   id                      INT            NOT NULL    UNIQUE,
+   username                VARCHAR(32)    NOT NULL    UNIQUE,
+   email_address           VARCHAR(64)    NOT NULL    UNIQUE,
+   phone_number            VARCHAR(16)                UNIQUE,
    hashed_password         CHAR(64)       NOT NULL,
    is_male                 BOOLEAN        NOT NULL    DEFAULT TRUE,
    username_last_updated   DATETIME       NOT NULL    DEFAULT CURRENT_TIMESTAMP,
 
-   CONSTRAINT primary_key_users PRIMARY KEY (id)
+   CONSTRAINT primary_key_users PRIMARY KEY (id),
+   CONSTRAINT foreign_key_users_id FOREIGN KEY (id) REFERENCES dietary_filters(id)
 );

05d03ceb9090c33b1f31121b5b90af2caf5a556d
Ahmad Mzayek <ahmadmzayek.cs@gmail.com>
2025-01-25 22:44:19 +0200

    - Removed "AUTO_INCREMENT" from the column "id". - Added "UNIQUE" to the columns "id" and "category". - Adjusted Foreign keys.

--- models/meals/meals_creation.sql
+++ models/meals/meals_creation.sql
@@ -1,26 +1,15 @@
 CREATE TABLE IF NOT EXISTS meals
 (
-   id							         INT			   NOT NULL    AUTO_INCREMENT,
-   category_id                   INT            NOT NULL,
+   id							         INT			   NOT NULL    UNIQUE,
+   category                      VARCHAR(32)    NOT NULL    UNIQUE,
    name							      VARCHAR(64)    NOT NULL,
    image_name						   VARCHAR(64)    NOT NULL    DEFAULT "default_meal_image.png",
    description					      TEXT		      NOT NULL,
    nb_portions					      INT			   NOT NULL,
    nb_calories_per_portion		   INT			   NOT NULL,
    preparation_duration_minutes  INT		      NOT NULL,
-   is_halal						      BOOLEAN        NOT NULL    DEFAULT FALSE,
-   is_kosher					      BOOLEAN        NOT NULL    DEFAULT FALSE,
-   is_organic					      BOOLEAN        NOT NULL    DEFAULT FALSE,
-   is_vegan						      BOOLEAN        NOT NULL    DEFAULT FALSE,
-   is_vegetarian				      BOOLEAN        NOT NULL    DEFAULT FALSE,
-   is_sugar_free				      BOOLEAN        NOT NULL    DEFAULT FALSE,
-   is_dairy_free				      BOOLEAN        NOT NULL    DEFAULT FALSE,
-   is_low_carb					      BOOLEAN        NOT NULL    DEFAULT FALSE,
-   is_low_calorie				      BOOLEAN        NOT NULL    DEFAULT FALSE,
-   is_low_sodium				      BOOLEAN        NOT NULL    DEFAULT FALSE,
-   is_high_protein			      BOOLEAN        NOT NULL    DEFAULT FALSE,
-   is_keto_friendly			      BOOLEAN        NOT NULL    DEFAULT FALSE,
 
    CONSTRAINT primary_key_meals PRIMARY KEY (id),
-   CONSTRAINT foreign_key_meals_category_id FOREIGN KEY (category_id) REFERENCES meal_categories(id)
+   CONSTRAINT foreing_key_meals_id FOREIGN KEY (id) REFERENCES dietary_filters(id),
+   CONSTRAINT foreign_key_meals_category FOREIGN KEY (category) REFERENCES meal_categories(name)
 );

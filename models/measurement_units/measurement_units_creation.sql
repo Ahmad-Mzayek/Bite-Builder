3921c99eaac9ccb7752c065f91d73f4f6ba4a5c0
Ahmad Mzayek <ahmadmzayek.cs@gmail.com>
2025-01-25 22:31:03 +0200

    - Changed types from "VARCHAR(16)" to "VARCHAR(32)".

--- models/measurement_units/measurement_units_creation.sql
+++ models/measurement_units/measurement_units_creation.sql
@@ -1,7 +1,7 @@
 CREATE TABLE IF NOT EXISTS measurement_units
 (
-    name_singular   VARCHAR(16)     NOT NULL    UNIQUE,
-    name_plural     VARCHAR(16)     NOT NULL    UNIQUE,
+    name_singular   VARCHAR(32)     NOT NULL    UNIQUE,
+    name_plural     VARCHAR(32)     NOT NULL    UNIQUE,
 
     CONSTRAINT primary_key_measurement_units PRIMARY KEY (name_singular)
 );

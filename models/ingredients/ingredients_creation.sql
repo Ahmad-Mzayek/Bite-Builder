55d00cc57706a3dc611c3d51b9fd8ef68575001a
Ahmad Mzayek <ahmadmzayek.cs@gmail.com>
2025-01-25 18:01:42 +0200

    - Replace the column "unit" with "measurement_unit_id", a foreign key that references the primary key of the table "measurement_units". - Added "UNIQUE" to the column "name".

--- models/ingredients/ingredients_creation.sql
+++ models/ingredients/ingredients_creation.sql
@@ -1,10 +1,11 @@
 CREATE TABLE IF NOT EXISTS ingredients
 (
-   id          INT            NOT NULL    AUTO_INCREMENT,
-   type_id     INT            NOT NULL,
-   name        VARCHAR(32)    NOT NULL,
-   unit        VARCHAR(16)    NOT NULL,
+   id                   INT            NOT NULL    AUTO_INCREMENT,
+   type_id              INT            NOT NULL,
+   measurement_unit_id  INT,
+   name                 VARCHAR(32)    NOT NULL    UNIQUE,
 
    CONSTRAINT primary_key_ingredients PRIMARY KEY (id),
-   CONSTRAINT foreign_key_ingredients_type_id FOREIGN KEY (type_id) REFERENCES ingredient_types(id)
+   CONSTRAINT foreign_key_ingredients_type_id FOREIGN KEY (type_id) REFERENCES ingredient_types(id),
+   CONSTRAINT foreign_key_ingredients_measurement_unit_id FOREIGN KEY (measurement_unit_id) REFERENCES measurement_units(id)
 );

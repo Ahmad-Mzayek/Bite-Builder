bcda8c0865862ecb134d16f7d9ce0b150086a57c
Ahmad Mzayek <ahmadmzayek.cs@gmail.com>
2025-01-25 23:09:32 +0200

    - Fixed typo.

--- models/recipes/recipes_creation.sql
+++ models/recipes/recipes_creation.sql
@@ -1,10 +1,10 @@
 CREATE TABLE IF NOT EXISTS recipes
 (
-   meal_name         VARCHAR(64)   NOT NULL,
+   meal_id           INT           NOT NULL,
    ingredient_name   VARCHAR(32)   NOT NULL,
    quantity          INT,
 
    CONSTRAINT primary_key_recipes PRIMARY KEY (meal_name, ingredient_name),
-   CONSTRAINT foreign_key_recipes_meal_name FOREIGN KEY (meal_name) REFERENCES meals(name),
+   CONSTRAINT foreign_key_recipes_meal_id FOREIGN KEY (meal_id) REFERENCES meals(id),
    CONSTRAINT foreign_key_recipes_ingredient_name FOREIGN KEY (ingredient_name) REFERENCES ingredients(name)
 );

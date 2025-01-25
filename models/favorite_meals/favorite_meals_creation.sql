f71fa5b9f6763724a3f7b9cd3ee1745da4f6348e
Ahmad Mzayek <ahmadmzayek.cs@gmail.com>
2025-01-25 22:54:51 +0200

    - Renamed table "favorite_meals" to "favorite".

--- models/favorite_meals/favorite_meals_creation.sql
+++
@@ -1,9 +0,0 @@
-CREATE TABLE IF NOT EXISTS favorite_meals
-(
-   user_id     INT   NOT NULL,
-   meal_id     INT   NOT NULL,
-
-   CONSTRAINT primary_key_favorite_meals PRIMARY KEY (user_id, meal_id),
-   CONSTRAINT foreign_key_favorite_meals_user_id FOREIGN KEY (user_id) REFERENCES users(id),
-   CONSTRAINT foreign_key_favorite_meals_meal_id FOREIGN KEY (meal_id) REFERENCES meals(id)
-);
---
+++ models/favorites/favorites_creation.sql
@@ -0,0 +1,9 @@
+CREATE TABLE IF NOT EXISTS favorites
+(
+   user_id     INT   NOT NULL,
+   meal_id     INT   NOT NULL,
+
+   CONSTRAINT primary_key_favorites PRIMARY KEY (user_id, meal_id),
+   CONSTRAINT foreign_key_favorites_user_id FOREIGN KEY (user_id) REFERENCES users(id),
+   CONSTRAINT foreign_key_favorites_meal_id FOREIGN KEY (meal_id) REFERENCES meals(id)
+);

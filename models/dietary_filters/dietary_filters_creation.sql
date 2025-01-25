48b0c353b6b5d46fa5e7319ebc3feffafe94586a
Ahmad Mzayek <ahmadmzayek.cs@gmail.com>
2025-01-25 22:41:07 +0200

    - Added the file "dietary_filters_creation.sql".

---
+++ models/dietary_filters/dietary_filters_creation.sql
@@ -0,0 +1,18 @@
+CREATE TABLE IF NOT EXISTS dietary_filters
+(
+    is_user_diet        BOOLEAN     NOT NULL,
+    id                  INT         NOT NULL    AUTO_INCREMENT,
+    is_halal		    BOOLEAN     NOT NULL    DEFAULT FALSE,
+    is_organic			BOOLEAN     NOT NULL    DEFAULT FALSE,
+    is_vegan			BOOLEAN     NOT NULL    DEFAULT FALSE,
+    is_vegetarian		BOOLEAN     NOT NULL    DEFAULT FALSE,
+    is_sugar_free		BOOLEAN     NOT NULL    DEFAULT FALSE,
+    is_dairy_free		BOOLEAN     NOT NULL    DEFAULT FALSE,
+    is_low_carb			BOOLEAN     NOT NULL    DEFAULT FALSE,
+    is_low_calorie		BOOLEAN     NOT NULL    DEFAULT FALSE,
+    is_low_sodium		BOOLEAN     NOT NULL    DEFAULT FALSE,
+    is_high_protein     BOOLEAN     NOT NULL    DEFAULT FALSE,
+    is_keto_friendly	BOOLEAN     NOT NULL    DEFAULT FALSE,
+
+    CONSTRAINT primary_key_dietary_filters PRIMARY KEY (is_user_diet, id)
+);

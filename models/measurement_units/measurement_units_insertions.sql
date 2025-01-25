5621e141820fa6e1fe45da65ef82d1e695af6e50
Ahmad Mzayek <ahmadmzayek.cs@gmail.com>
2025-01-25 17:57:18 +0200

    - Added the file "measurement_units_insertions.sql". - Added some insertion queries for testing.

---
+++ models/measurement_units/measurement_units_insertions.sql
@@ -0,0 +1,8 @@
+INSERT INTO measurement_units(name_singular, name_plural) VALUES
+("Can", "Cans"),
+("Cube", "Cubes"),
+("Gram", "Grams"),
+("Head", "Heads"),
+("Leaf", "Leaves"),
+("Piece", "Pieces"),
+("Milliliter", "Milliliters");

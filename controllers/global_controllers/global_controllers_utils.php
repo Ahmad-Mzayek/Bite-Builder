319ade907869341c12b1878a9cc5d2445ee4bf3c
Ahmad Mzayek <ahmadmzayek.cs@gmail.com>
2025-01-29 10:34:21 +0200

    - Improved "start_user_session()" function.

--- controllers/global_controllers/global_controllers_utils.php
+++ controllers/global_controllers/global_controllers_utils.php
@@ -9,15 +9,28 @@ function execute_statement(mysqli_stmt $statement) : void // -------------------
     }
 }
 
-function start_user_session(string $username) : void // -----------------------------------------------------------------------
+function start_user_session(int $id): void // ---------------------------------------------------------------------------------
 {
-    session_start();
-    session_regenerate_id(true);
-    $_SESSION["username"] = $username;
+    if (session_status() === PHP_SESSION_NONE)
+    {
+        session_set_cookie_params([
+            "lifetime" => 0,
+            "path" => "/",
+            "domain" => "",
+            "secure" => true,
+            "httponly" => true,
+            "samesite" => "Strict"
+        ]);
+        if (!session_start())
+            throw new Exception("Failed to start the session.");
+        if (!session_regenerate_id(true))
+            throw new Exception("Failed to regenerate session ID.");
+    }
+    $_SESSION["id"] = $id;
     $_SESSION["user_logged_in"] = true;
 }
 
-function send_response(string $status, string $message) : void // -------------------------------------------------------------
+function send_response(string $status, string|array $message) : void // -------------------------------------------------------
 {
     $response = ["status" => $status, "message" => $message];
     echo json_encode($response);

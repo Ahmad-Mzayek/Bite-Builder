29206bbbc8773849c07a1e037e42c8d008ea21b8
amirbg2004 <amirbg43@hotmail.com>
2025-01-31 20:57:28 +0200

    Added intial shopping list section filled with test text.

--- views/meal_page_views/php/shopping_list.php
+++ views/meal_page_views/php/shopping_list.php
@@ -1,5 +1,9 @@
-<div>
-	<div id="shopping-list-item-name"></div>
-	<div id="shopping-list-item-quantity"></div>
-	<div id="shopping-list-item-unit"></div>
+<div class="basis-1/4 h-full ">
+	<div class="grid grid-cols-3 items-center justify-between w-full h-full p-4 border-2 border-gray-950">
+		<div id="shopping-list-ingredient-name">ingredient Name</div>
+
+		<div id="shopping-list-ingredient-quantity">Ingredient Quantity</div>
+
+		<div id="shopping-list-ingredient-unit">Ingredient Unit</div>
+	</div>
 </div>

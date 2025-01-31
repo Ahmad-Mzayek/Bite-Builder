23ddb8e0433ac52859ec43b5219d655c220e7cb3
amirbg2004 <amirbg43@hotmail.com>
2025-01-31 20:47:02 +0200

    Added some global utility functions particularly to assist with the loading animation.

--- views/global_views/javascript/global_utils.js
+++ views/global_views/javascript/global_utils.js
@@ -3,6 +3,10 @@ export const toggleVisibility = (element, show) => {
   element.classList.toggle("flex", show);
 };
 
+export const togglePointerEvents = (element, disable) => {
+  element.classList.toggle("pointer-events-none", disable);
+};
+
 export const switchElements = (elementToShow, elementToHide) => {
   toggleVisibility(elementToHide, false);
   toggleVisibility(elementToShow, true);
@@ -10,4 +14,20 @@ export const switchElements = (elementToShow, elementToHide) => {
 
 export const deleteInputData = (...inputs) => {
   inputs.forEach((input) => (input.value = ""));
+};
+
+export const isOverlayVisible = (overlay) => {
+  return overlay.classList.contains("flex");
+};
+
+export const isAnyPopupVisible = (...popups) => {
+  return popups.some((popup) => {
+    return popup.classList.contains("flex");
+  });
+};
+
+export const toggleLoadingAnimation = (overlay, loadingAnimationSpinner, isLoading) => {
+  toggleVisibility(overlay, isLoading);
+  overlay.classList.replace("z-10", "z-30");
+  toggleVisibility(loadingAnimationSpinner, isLoading);
 };

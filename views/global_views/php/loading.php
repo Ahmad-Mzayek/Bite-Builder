e125611f7a134d30ba99138228c90d7314501980
amirbg2004 <amirbg43@hotmail.com>
2025-01-31 20:47:25 +0200

    Added loading animation PHP file to global.

---
+++ views/global_views/php/loading.php
@@ -0,0 +1,6 @@
+<!-- From Uiverse.io by G4b413l -->
+<div id="loading-animation-spinner" class="leap-frog hidden absolute inset-0 m-auto z-50">
+	<div class="leap-frog__dot"></div>
+	<div class="leap-frog__dot"></div>
+	<div class="leap-frog__dot"></div>
+</div>

2a596212503abf7b6a99ae960ed9b32ec3eedfe2
Ahmad Mzayek <ahmadmzayek.cs@gmail.com>
2025-02-06 17:07:58 +0200

    - Added missing recipe for meal.

---
+++ models/recipes/recipes_insertions.sql
@@ -0,0 +1,9 @@
+INSERT INTO recipes
+VALUES
+(1, "Hot Dog", 2),
+(1, "Yellow Mustard", 200),
+(1, "Brown Sugar", 24),
+(1, "Cayenne Pepper", 2),
+(1, "Onion Powder", 2),
+(1, "Ground Black Pepper", 1),
+(1, "Barbecue Sauce", 200);

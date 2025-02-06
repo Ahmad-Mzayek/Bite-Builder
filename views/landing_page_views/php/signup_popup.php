7263df8857149e104dd9af6d2f18d7ef405e1ec4
amirbg2004 <amirbg43@hotmail.com>
2025-02-06 12:51:27 +0200

    Made some code alignment adjustments and removed some unneeded class attributes.

--- views/landing_page_views/php/signup_popup.php
+++ views/landing_page_views/php/signup_popup.php
@@ -1,68 +1,65 @@
 <?php include "../../global_views/php/show_password_icons.php"; ?>
 
 <div id="signup-popup"
-     class="hidden flex-col w-3/12 min-w-[25rem] min-h-[20rem] items-center justify-between fixed top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 z-20 shadow-xl rounded-lg p-6 bg-gray-200">
+     class="popup hidden flex-col w-3/12 min-w-[25rem] min-h-[20rem] items-center justify-between fixed top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 z-20 shadow-xl rounded-lg p-6 bg-gray-200">
     <h1 class="pb-4 font-bold">
         Sign Up
     </h1>
-    <svg id="close-signup-popup-svg"
-         class="absolute transition cursor-pointer right-4 top-3 hover:scale-110"
-         xmlns="http://www.w3.org/2000/svg"
-         width="32"
-         height="32"
-         viewBox="0 0 24 24">
-        <path fill="currentColor"
-              d="m12 13.4l-4.9 4.9q-.275.275-.7.275t-.7-.275t-.275-.7t.275-.7l4.9-4.9l-4.9-4.9q-.275-.275-.275-.7t.275-.7t.7-.275t.7.275l4.9 4.9l4.9-4.9q.275-.275.7-.275t.7.275t.275.7t-.275.7L13.4 12l4.9 4.9q.275.275.275.7t-.275.7t-.7.275t-.7-.275z" />
-    </svg>
-    <form id="signup-form"
-          class="flex flex-col items-center w-full space-y-2">
-        <div id="signup-error-container"
-             class="hidden w-full p-2 font-bold text-white bg-red-500">
+    
+    <?php include("../../global_views/php/close-popup-svg.php"); ?>
+    
+    <form id="signup-form" class="flex flex-col items-center w-full space-y-2">
+        <div id="signup-error-container" class="hidden w-full p-2 font-bold text-white bg-red-500">
+
         </div>
+
         <input type="text"
-               name="username_input"
                id="signup-username-input"
+               name="username_input"
                placeholder="Username"
                autocomplete="username"
                minlength="8"
                required
                maxlength="32">
+
         <input type="email"
-               name="email_address_input"
                id="signup-email-input"
+               name="email_address_input"
                placeholder="Email Address"
-               required
                autocomplete="email"
                minlength="1"
-               maxlength="64">
+               maxlength="64"
+               required>
+
         <div class="relative w-full cursor-pointer">
             <input type="password"
-                   class="password-input"
-                   name="password_input"
                    id="signup-password-input"
+                   name="password_input"
                    placeholder="Password"
-                   required
                    minlength="8"
-                   maxlength="64">
+                   maxlength="64"
+                   required>
+
             <span class="absolute inset-y-0 right-0 flex items-center pr-3 cursor-pointer toggle-password-icon">
                 <?php echo $show_password_false_icon; ?>
             </span>
         </div>
         <div class="relative w-full cursor-pointer">
             <input type="password"
-                   class="confirm-password-input"
-                   name="confirm_password_input"
                    id="signup-confirm-password-input"
+                   name="confirm_password_input"
                    placeholder="Confirm Password"
-                   required
-                   maxlength="64">
+                   maxlength="64"
+                   required>
+
             <span class="absolute inset-y-0 right-0 flex items-center pr-3 cursor-pointer toggle-password-icon">
                 <?php echo $show_password_false_icon; ?>
             </span>
         </div>
-        <input type="submit"
-               value="SIGNUP">
+
+        <input type="submit" value="SIGNUP">
     </form>
+    
     <h3>
         Already Have An Account?
         <button id="show-login-button"

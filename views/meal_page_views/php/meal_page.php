20f4419d6983a6e614d68e9a061f020d9e9a7089
amirbg2004 <amirbg43@hotmail.com>
2025-02-06 12:57:58 +0200

    Included some added popups, adjusted body padding, and added session start function at the start.

--- views/meal_page_views/php/meal_page.php
+++ views/meal_page_views/php/meal_page.php
@@ -1,3 +1,9 @@
+<?php
+	session_start();
+	// if(!$_SESSION["user_id"]) 
+	// 	header("Location: ../../landing_page_views/php/landing_page.php");
+?> 
+
 <!DOCTYPE html>
 <html lang="en">
 	<head>
@@ -16,12 +22,14 @@
 		include("../../global_views/php/overlay.php");
 		include("./dropdown_menu_overlay.php");
 		include("./profile_popup.php");
-		include("./meal_filters_popup.php");
+		include("./delete_account_confirmation_popup.php");
+		include("./change_password_popup.php");
+		include("./preferences_popup.php");
 		include("../../global_views/php/loading.php");
 		?>
 		<div class="flex flex-col items-center justify-between h-screen">
 			<?php include("./meal_page_header.php"); ?>
-			<div class="flex w-screen min-h-[65rem] p-8 items-center">
+			<div class="flex w-full px-3 py-4 items-center">
 				<?php
 				include("./meal_page_body.php");
 				include("./shopping_list.php");

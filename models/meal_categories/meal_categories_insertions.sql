27ecb141c541e9f74da6cb4db7069a049e12cbd7
amirbg2004 <amirbg43@hotmail.com>
2025-02-07 22:30:38 +0200

    [meal_categories_insertions.sql] Added some categories.

--- models/meal_categories/meal_categories_insertions.sql
+++ models/meal_categories/meal_categories_insertions.sql
@@ -1,4 +1,11 @@
-INSERT INTO meal_categories(category_name)
+INSERT IGNORE INTO meal_categories(category_name)
 VALUES
+("Salads"),
+("Desserts"),
+("Breakfasts"),
+("Soups & Stews"),
+("Main Courses"),
+("Bread & Pastries"),
+("Sauces & Condiments"),
 ("Drinks"),
 ("Appetizers");

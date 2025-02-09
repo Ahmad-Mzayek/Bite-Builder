b1aa5a86a0ec6eaf7d054ef4d29090458534e881
amirbg2004 <amirbg43@hotmail.com>
2025-02-10 01:01:12 +0200

    Edited footer top margin and added code spacing for better readability.

--- views/global_views/php/footer.php
+++ views/global_views/php/footer.php
@@ -1,12 +1,22 @@
-<footer class="flex *:text-white flex-col w-screen items-center justify-between p-8 pr-12 bg-slate-600">
+<footer class="flex flex-col w-screen items-center justify-between p-6 pr-12 mt-16">
    <h1 class="pb-6 lg:text-5xl">
       Developers
    </h1>
+
    <div class="flex items-center justify-around w-full pt-2 pb-2">
-      <h2>LinkedIn</h2>
-      <h2>GitHub</h2>
-      <h2>Email</h2>
+      <h2>
+         LinkedIn
+      </h2>
+
+      <h2>
+         GitHub
+      </h2>
+
+      <h2>
+         Email
+      </h2>
    </div>
+
    <div class="flex items-center justify-around w-full pt-4">
       <div class="flex flex-col items-center justify-around w-full [&>*]:transition ml-4">
          <a class="hover:underline"
@@ -14,45 +24,53 @@
             href="https://www.linkedin.com/in/ahmad-mzayek-97817330b">
             Ahmad Mzayek
          </a>
+
          <a class="hover:underline"
             target="_blank"
             href="https://www.linkedin.com/in/carla-kinaan-4ba434333/">
             Carla Kinaan
          </a>
+
          <a class="hover:underline"
             target="_blank"
             href="https://www.linkedin.com/in/amir-bou-ghanem">
             Amir Bou Ghanem
          </a>
       </div>
+
       <div class="flex flex-col items-center justify-around w-full ml-4 border-l-2 border-r-2">
          <a class="hover:underline"
             target="_blank"
             href="https://www.github.com/Ahmad-Mzayek">
             Ahmad Mzayek
          </a>
+
          <a class="hover:underline"
             target="_blank"
             href="https://github.com/CarlaKinaan">
             Carla Kinaan
          </a>
+
          <a class="hover:underline"
             target="_blank"
             href="https://www.github.com/amirbg2004">
             Amir Bou Ghanem
          </a>
       </div>
+
       <div class="flex flex-col items-center justify-around w-full">
          <a class="hover:underline"
             target="_blank"
             href="mailto:ahmadmzayek.cs@gmail.com">
             Ahmad Mzayek
          </a>
+
          <a class="hover:underline"
             target="_blank"
             href="mailto:carla.gms209@gmail.com">
             Carla Kinaan
          </a>
+
          <a class="hover:underline"
             target="_blank"
             href="mailto:amirbg43@hotmail.com">

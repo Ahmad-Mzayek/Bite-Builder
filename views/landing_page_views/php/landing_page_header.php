1bb2b69d1ff7509adb020e0809e65caab7b3463f
amirbg2004 <amirbg43@hotmail.com>
2025-02-11 01:50:51 +0200

    Lowered landing page header height and cleaned some classes.

--- views/landing_page_views/php/landing_page_header.php
+++ views/landing_page_views/php/landing_page_header.php
@@ -1,11 +1,11 @@
-<header class="fixed flex items-center justify-between w-screen px-8 z-[5] top-0">
-	<div class="flex items-center justify-center w-[7rem]">
-		<img id="logo-image" class="w-full h-full object-cover" src="../../../resources/images/logo_dark.png" alt="Logo">
+<header class="fixed flex items-center justify-between w-screen px-8 z-[5] top-0 h-20">
+	<div class="flex items-center justify-center h-full">
+		<img id="logo-image" class="object-cover w-full h-full" src="../../../resources/images/logo_dark.png" alt="Logo">
 	</div>
 
-	<div class="flex flex-wrap items-center space-x-12 md:flex-nowrap lg:basis-2/12">
+	<div class="flex flex-wrap items-center space-x-12 md:flex-nowrap ">
 		<button id="login-signup-button"
-                class="p-6 font-semibold transition rounded-3xl hover:scale-95">
+                class="p-3 px-6 font-semibold transition rounded-3xl hover:scale-95">
 			Log In / Sign Up
 		</button>
 

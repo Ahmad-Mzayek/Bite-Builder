fdce9d8fe0cf9d2ba5ac435e02146219701579f7
Ahmad Mzayek <ahmadmzayek.cs@gmail.com>
2025-02-10 07:40:51 +0200

    - Added a procedure to fetch a user's meal categories. - Fixed "UserInfoController.php" to send all available categories.

--- models/shopping_lists/fetch_shopping_list_procedure.sql
+++ models/shopping_lists/fetch_shopping_list_procedure.sql
@@ -1,24 +1,24 @@
 DELIMITER //
 CREATE PROCEDURE fetch_shopping_list(IN user_id_input INT)
 BEGIN
-    CREATE TEMPORARY TABLE IF NOT EXISTS temp_shopping_list_view AS
+    CREATE TEMPORARY TABLE IF NOT EXISTS temp_shopping_list AS
         SELECT ingredient_name, quantity
         FROM shopping_lists
         WHERE user_id = user_id_input;
 
-    CREATE TEMPORARY TABLE IF NOT EXISTS temp_ingredients_view AS
-        SELECT ing.*, slv.quantity
-        FROM temp_shopping_list_view AS slv
+    CREATE TEMPORARY TABLE IF NOT EXISTS temp_ingredients AS
+        SELECT ing.*, tsl.quantity
+        FROM temp_shopping_list AS tsl
         JOIN ingredients AS ing
-            ON slv.ingredient_name = ing.ingredient_name;
+            ON tsl.ingredient_name = ing.ingredient_name;
 
-    SELECT ingv.*, mu.unit_name_plural
-    FROM temp_ingredients_view AS ingv
+    SELECT ti.*, mu.unit_name_plural
+    FROM temp_ingredients AS ti
     JOIN measurement_units AS mu
-        ON ingv.unit_name_singular = mu.unit_name_singular;
+        ON ti.unit_name_singular = mu.unit_name_singular;
 
-    DROP TEMPORARY TABLE IF EXISTS temp_shopping_list_view;
-    DROP TEMPORARY TABLE IF EXISTS temp_ingredients_view;
+    DROP TEMPORARY TABLE IF EXISTS temp_shopping_list;
+    DROP TEMPORARY TABLE IF EXISTS temp_ingredients;
 END;
 //
 DELIMITER ;
---
+++ models/users_meal_categories/fetch_user_meal_categories_procedure.sql
@@ -0,0 +1,21 @@
+DELIMITER //
+CREATE PROCEDURE fetch_user_meal_categories_procedure(IN user_id_input INT)
+BEGIN
+    CREATE TEMPORARY TABLE IF NOT EXISTS temp_user_categories AS
+        SELECT category_name
+        FROM users_meal_categories
+        WHERE user_id = user_id_input;
+
+    SELECT mc.category_name,
+        CASE
+            WHEN tuc.category_name IS NOT NULL THEN TRUE
+            ELSE FALSE
+        END AS is_selected
+    FROM meal_categories AS mc
+    LEFT JOIN temp_user_categories AS tuc
+    ON mc.category_name = tuc.category_name;
+
+    DROP TEMPORARY TABLE IF EXISTS temp_user_categories;
+END;
+//
+DELIMITER ;

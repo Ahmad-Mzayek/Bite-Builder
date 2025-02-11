a3e7778eeb14e88a797019bd587bc587b843b78f
Ahmad Mzayek <ahmadmzayek.cs@gmail.com>
2025-02-11 07:30:02 +0200

    - Added a trigger that inserts a newly inserted meal category into all users users_meal_categories rows.

---
+++ models/meal_categories/after_meal_category_insert_trigger.sql
@@ -0,0 +1,34 @@
+DELIMITER //
+CREATE TRIGGER IF NOT EXISTS after_meal_category_insert_trigger
+AFTER INSERT ON meal_categories
+FOR EACH ROW
+BEGIN
+    DECLARE current_user_id INT;
+    DECLARE done BOOLEAN DEFAULT FALSE;
+    
+    DECLARE users_cursor CURSOR FOR
+        SELECT user_id
+        FROM users;
+
+    DECLARE CONTINUE HANDLER
+    FOR NOT FOUND
+    SET done = TRUE;
+
+    OPEN users_cursor;
+
+    for_each_user_loop: LOOP
+        FETCH users_cursor
+        INTO current_user_id;
+        
+        IF done THEN
+            LEAVE for_each_user_loop;
+        END IF;
+        
+        INSERT INTO users_meal_categories
+        VALUES (current_user_id, NEW.category_name);
+    END LOOP;
+
+    CLOSE users_cursor;
+END
+//
+DELIMITER ;

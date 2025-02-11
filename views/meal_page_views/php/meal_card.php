edc7b215622464507cc1c8fb90c9f90abe6280cb
amirbg2004 <amirbg43@hotmail.com>
2025-02-12 00:47:23 +0200

    Changed image in card from contain to cover.

--- views/meal_page_views/php/meal_card.php
+++ views/meal_page_views/php/meal_card.php
@@ -1,4 +1,3 @@
 <img id="meal-image"
-     class="object-contain  aspect-square"
-     src="../../../resources/images/default_meal_image.png"
+     class="object-cover aspect-square"
      alt="Meal Image">

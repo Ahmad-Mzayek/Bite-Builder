4b8e1f2e7a834f7b4b1e1e3a6484206d01577629
Ahmad Mzayek <ahmadmzayek.cs@gmail.com>
2025-02-11 07:19:10 +0200

    - Added all meal categories to "users_meal_categories" table on successful signup by using a MySQL cursor in "user_insertion_procedure.sql".

--- models/procedures/users_insertion_procedure.sql
+++ models/procedures/users_insertion_procedure.sql
@@ -9,10 +9,38 @@ BEGIN
     INSERT INTO dietary_filters
     VALUES ();
     
-    SET @filters_id = LAST_INSERT_ID();
+    SET @user_id = LAST_INSERT_ID();
     
     INSERT INTO users(user_id, username, email_address, hashed_password)
-    VALUES (@filters_id, username_input, email_address_input, hashed_password_input);
+    VALUES (@user_id, username_input, email_address_input, hashed_password_input);
+
+    DECLARE done BOOLEAN DEFAULT FALSE;
+
+    DECLARE current_category_name VARCHAR(32);
+
+    DECLARE meal_categories_cursor CURSOR FOR
+        SELECT category_name
+        FROM meal_categories;
+
+    DECLARE CONTINUE HANDLER
+    FOR NOT FOUND
+    SET done = TRUE;
+    
+    OPEN meal_categories_cursor;
+
+    for_each_meal_category_loop: LOOP
+        FETCH meal_categories_cursor
+        INTO current_category_name;
+
+        IF done THEN
+            LEAVE for_each_meal_category_loop;
+        END IF;
+
+        INSERT INTO users_meal_categories
+        VALUES (@user_id, current_category_name);
+    END LOOP;
+
+    CLOSE meal_categories_cursor;
 END;
 //
 DELIMITER ;

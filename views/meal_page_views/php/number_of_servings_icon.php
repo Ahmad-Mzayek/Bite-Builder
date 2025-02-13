2c986514851d467ab8a4968ce2322e3c926dd453
amirbg2004 <amirbg43@hotmail.com>
2025-02-13 12:49:21 +0200

    Downsized some meal body icons.

--- views/meal_page_views/php/number_of_servings_icon.php
+++ views/meal_page_views/php/number_of_servings_icon.php
@@ -1,4 +1,4 @@
-<svg class="w-12 h-12 opacity-100 fill-white" 
+<svg class="w-10 h-10 opacity-100 fill-white" 
 		 xmlns="http://www.w3.org/2000/svg"
 		 xmlns:xlink="http://www.w3.org/1999/xlink" 
 	 	 viewBox="0 0 512 512"  xml:space="preserve">

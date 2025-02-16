13fd66a170ea1f4891429aa317ed81b8d1386160
amirbg2004 <amirbg43@hotmail.com>
2025-02-17 00:44:11 +0200

    Adjusted meal details max height from rem to viewport height.

--- views/meal_page_views/php/meal_details_popup.php
+++ views/meal_page_views/php/meal_details_popup.php
@@ -1,5 +1,5 @@
 <div id="meal-details-popup"
-     class="popup hidden flex-col items-center max-h-[45rem] gap-4 fixed top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 z-20 shadow-2xl rounded-lg p-6 py-6 overflow-x-hidden overflow-y-auto">
+     class="popup hidden flex-col items-center max-h-[80vh] gap-4 fixed top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 z-20 shadow-2xl rounded-lg p-6 py-6 overflow-x-hidden overflow-y-auto">
 		<h1 class="font-bold">
 				Meal Details
 		</h1>

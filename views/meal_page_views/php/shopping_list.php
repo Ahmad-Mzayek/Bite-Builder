0243c91e5e1f48120d3e89a0718b663dd4118e28
amirbg2004 <amirbg43@hotmail.com>
2025-02-17 00:46:00 +0200

    Removed shopping list padding and changed max height.

--- views/meal_page_views/php/shopping_list.php
+++ views/meal_page_views/php/shopping_list.php
@@ -1,9 +1,13 @@
-	<div id="shopping-list" class="flex flex-col flex-1 basis-4/12 overflow-auto max-h-[45rem]">
-		<h1 class="pb-3 text-center">
-			Shopping List
-		</h1>
+	<div id="shopping-list" class="flex flex-col flex-1 max-h-[80vh] basis-4/12 overflow-y-scroll">
+		<div class="p-1 relative">
+			<?php include("./clear_shopping_list_icon.php"); ?>
 
-		<div id="shopping-list-grid-container" class="grid grid-cols-4 p-2 border-2 place-items-start justify-items-center h-full grow border-white-950 text-nowrap overflow-y-auto">
+			<h1 class="pb-3 text-center">
+				Shopping List
+			</h1>
+		</div>
+
+		<div id="shopping-list-grid-container" class="grid grid-cols-4 p-0 border-2 place-items-start justify-items-center h-full grow border-white-950 text-nowrap overflow-y-auto">
 			<h3>Ingredient Name</h3>
 
 			<h3 class="col-span-2">Ingredient Quantity</h3>

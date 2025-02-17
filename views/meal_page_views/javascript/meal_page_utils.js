dbb337977c6db8baa10ab97bb74781cfee175354
amirbg2004 <amirbg43@hotmail.com>
2025-02-17 23:36:34 +0200

    Added right padding to shopping list ingredient containers and downsized the delete ingredient button.

--- views/meal_page_views/javascript/meal_page_utils.js
+++ views/meal_page_views/javascript/meal_page_utils.js
@@ -356,7 +356,7 @@ export const addMealIngredientsToShoppingList = (shoppingList, shoppingListGridC
     ingredientContainer.classList.add("flex", "items-center", "gap-2", "p-2", "w-full", "col-span-4");
     ingredientContainer.innerHTML = `
       <h4 class="flex items-center justify-start gap-3 text-[1rem] basis-full text-wrap">
-        <svg class="delete-ingredient-icon w-7 h-7 cursor-pointer" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 448 512">
+        <svg class="delete-ingredient-icon w-6 h-6 cursor-pointer" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 448 512">
         <path d="M170.5 51.6L151.5 80l145 0-19-28.4c-1.5-2.2-4-3.6-6.7-3.6l-93.7 0c-2.7 0-5.2 1.3-6.7 3.6zm147-26.6L354.2 80 368 80l48 0 8 0c13.3 0 24 10.7 24 24s-10.7 24-24 24l-8 0 0 304c0 44.2-35.8 80-80 80l-224 0c-44.2 0-80-35.8-80-80l0-304-8 0c-13.3 0-24-10.7-24-24S10.7 80 24 80l8 0 48 0 13.8 0 36.7-55.1C140.9 9.4 158.4 0 177.1 0l93.7 0c18.7 0 36.2 9.4 46.6 24.9zM80 128l0 304c0 17.7 14.3 32 32 32l224 0c17.7 0 32-14.3 32-32l0-304L80 128zm80 64l0 208c0 8.8-7.2 16-16 16s-16-7.2-16-16l0-208c0-8.8 7.2-16 16-16s16 7.2 16 16zm80 0l0 208c0 8.8-7.2 16-16 16s-16-7.2-16-16l0-208c0-8.8 7.2-16 16-16s16 7.2 16 16zm80 0l0 208c0 8.8-7.2 16-16 16s-16-7.2-16-16l0-208c0-8.8 7.2-16 16-16s16 7.2 16 16z"/>
         </svg>
 
@@ -389,7 +389,7 @@ export const addMealIngredientsToShoppingList = (shoppingList, shoppingListGridC
         </button>       
       </div>
 
-      <h4 class="text-[1.2rem] basis-1/2 text-end">
+      <h4 class="pr-2 text-[1.2rem] basis-1/2 text-end">
         ${ingredient.quantity === 1 || ingredient.quantity === null ? ingredient.unit_name_singular : ingredient.unit_name_plural}
       </h4>
     </div>

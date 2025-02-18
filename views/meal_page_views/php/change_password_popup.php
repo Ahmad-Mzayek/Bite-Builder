7134e012a2c26e3478b5ab20992581cb21e6299c
amirbg2004 <amirbg43@hotmail.com>
2025-02-18 23:33:26 +0200

    Made the change password popup form submit using <input type="submit"> to implement "Enter" key submission functionality.

--- views/meal_page_views/php/change_password_popup.php
+++ views/meal_page_views/php/change_password_popup.php
@@ -13,14 +13,13 @@
         Password Changed Successfully!
     </div>
 
-    <form class="flex flex-col w-full gap-8">
+    <form id="change-password-form" class="flex flex-col w-full gap-8">
         <div class="flex flex-col w-full gap-3">
             <div class="relative w-full">
                 <input type="password"
                        name="current_password_input"
                        placeholder="Current Password"
-                       maxlength="64"
-                       required>
+                       maxlength="64" >
 
                 <span class="absolute inset-y-0 right-0 flex items-center pr-3 cursor-pointer toggle-password-icon">
                     <?php echo $show_password_false_icon; ?>
@@ -55,13 +54,13 @@
         </div>
 
         <div class="flex items-center justify-center w-full space-x-12">
-            <button id="close-change-password-popup-button" class="h-12 font-bold text-white bg-green-700 rounded-lg w-36 hover:bg-green-800">
+            <button type="button" id="close-change-password-popup-button" class="h-12 font-bold text-white bg-red-700 rounded-lg w-36 hover:bg-red-800">
                 Cancel
             </button>
 
-            <button id="confirm-change-password-button" type="submit" class="h-12 font-bold text-white bg-red-700 rounded-lg w-36 hover:bg-red-800">
-                Change
-            </button>
+            <input type="submit" 
+                   class="h-12 font-bold text-white bg-green-700 rounded-lg w-36 hover:bg-green-800"
+                   value="Change" >
         </div>
     </form>
 </div>

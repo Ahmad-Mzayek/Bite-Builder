7d0300dbf3200b83d6e082e871849983bf06f1b2
amirbg2004 <amirbg43@hotmail.com>
2025-02-18 23:34:12 +0200

    Made the delete account confirmation popup form submit using <input type="submit"> to implement "Enter" key submission functionality.

--- views/meal_page_views/php/delete_account_confirmation_popup.php
+++ views/meal_page_views/php/delete_account_confirmation_popup.php
@@ -10,13 +10,14 @@
 
 		<p class="text-base font-bold text-red-600">Are you sure you want to delete your account? This action is irreversible!</p>
 
-			<div class="w-full flex flex-col items-center gap-10">
+			<form id="delete-account-form" class="w-full flex flex-col items-center gap-10">
 				<div class="relative w-full">
 					<input type="password"
-								class="current-password-input"
-								placeholder="Enter Your Current Password"
-								maxlength="64"
-								required>
+								 class="current-password-input"
+								 name="password_input"
+								 placeholder="Enter Your Current Password"
+								 maxlength="64"
+								 required>
 
 						<span class="absolute inset-y-0 right-0 flex items-center pr-3 cursor-pointer toggle-password-icon">
 								<?php echo $show_password_false_icon; ?>
@@ -24,13 +25,12 @@
 				</div>
 
 				<div class="flex flex-wrap items-center justify-center gap-6">
-					<button id="close-delete-account-confirmation-popup-button" class="h-12 font-bold text-white bg-green-700 rounded-lg w-36 hover:bg-green-800">
+					<button type="button" id="close-delete-account-confirmation-popup-button" class="h-12 font-bold text-white bg-green-700 rounded-lg w-36 hover:bg-green-800">
 						No
 					</button>
 
-					<button id="confirm-delete-account-button" class="h-12 font-bold text-white bg-red-700 rounded-lg w-36 hover:bg-red-800">
-						Yes
-					</button>
+					<input type="submit" value="Yes" class="h-12 font-bold text-white bg-red-700 rounded-lg w-36 hover:bg-red-800">
+						
 				</div>
-			</div>
+			</form>
 </div>

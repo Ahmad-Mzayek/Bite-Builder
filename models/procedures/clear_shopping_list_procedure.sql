e1cbfa93cdd9536f036885ccf646a89402e451b8
Ahmad Mzayek <ahmadmzayek.cs@gmail.com>
2025-02-18 18:43:36 +0200

    - Added a transaction inside "clear_shopping_list()" procedure.

--- models/procedures/clear_shopping_list_procedure.sql
+++ models/procedures/clear_shopping_list_procedure.sql
@@ -3,7 +3,16 @@ DROP PROCEDURE IF EXISTS clear_shopping_list;
 DELIMITER //
 CREATE PROCEDURE clear_shopping_list(IN user_id_input INT)
 BEGIN
+    DECLARE exit handler for sqlexception 
+    BEGIN
+        ROLLBACK;
+    END;
+
+    START TRANSACTION;
+
     DELETE FROM shopping_lists
     WHERE user_id = user_id_input;
+
+    COMMIT;
 END; //
 DELIMITER ;
